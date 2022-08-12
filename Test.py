9ee4cfbb8086e6468376bb6b5235ce4846270e33
JosephKemper <JosephKemper@gmail.com>
2022-08-12 17:35:36 -0600

    Completed work on 2 Assignments

--- Test.py
+++ Test.py
@@ -1,25 +1,19 @@
-# Test file to try different things I am learning
+from datetime import datetime
+current_date = datetime.now()
+print(current_date)
+print("Day: " + str(current_date.day))
+print("Month: " + str(current_date.month))
+print("Year: " + str(current_date.year))
 
 
-# Example 7
+# Call the now() method to get the current
+# date and time as a datetime object from
+# the computer's operating system.
+current_date_and_time = datetime.now()
 
-# Get the cost of an item from the user.
-cost = float(input("Please enter the cost: "))
+# Call the weekday() method to get the day of the
+# week from the current_date_and_time object.
+day_of_week = current_date_and_time.weekday()
 
-# Determine a discount rate based on the cost.
-if cost < 100:
-    rate = 0.10
-elif cost < 250:
-    rate = 0.15
-elif cost < 400:
-    rate = 0.18
-else:
-    rate = 0.20
-
-# Compute the discount amount
-# and the discounted cost.
-discount = cost * rate
-cost -= discount
-
-# Print the discounted cost for the user to see.
-print(f"After the discount, you will pay {cost:.2f}")
+# Print the day of the week for the user to see.
+print(day_of_week)
