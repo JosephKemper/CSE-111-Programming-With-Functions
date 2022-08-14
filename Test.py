d761eed7dc057b0abbefa57bad6675a948fe227f
JosephKemper <JosephKemper@gmail.com>
2022-08-13 18:55:16 -0600

    Finished 02 prove

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-current_date = datetime.now()
+from datetime import date
+current_date = date.today()
 print(current_date)
 print("Day: " + str(current_date.day))
 print("Month: " + str(current_date.month))
@@ -9,7 +9,7 @@ print("Year: " + str(current_date.year))
 # Call the now() method to get the current
 # date and time as a datetime object from
 # the computer's operating system.
-current_date_and_time = datetime.now()
+current_date_and_time = date.today()
 
 # Call the weekday() method to get the day of the
 # week from the current_date_and_time object.
