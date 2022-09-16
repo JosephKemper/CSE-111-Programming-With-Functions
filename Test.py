b510d6217f41286bd21543d7bfb2fadedab419a1
JosephKemper <JosephKemper@gmail.com>
2022-09-15 20:53:36 -0600

    Worked with group to complete Discount Assignment

--- Test.py
+++ Test.py
@@ -1,5 +1,9 @@
-a = 1
-b = 3
-c = -2
-result = a + b * 7 % 4 - c
-print(result)
+if week_day == 1 or week_day == 2:
+    print('you win a 10 percent discount')
+    discount = subtotal * 0.1
+    subtotal = subtotal * 0.9
+else:
+    print('Thanks for your shop')
+
+taxes = subtotal * 0.06
+total = taxes + subtotal
