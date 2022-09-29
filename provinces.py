b06d60964a5f00fa18c154e20c42400399e8bab0
JosephKemper <JosephKemper@gmail.com>
2022-09-28 20:58:43 -0600

    Syncing Work

---
+++ provinces.py
@@ -0,0 +1,15 @@
+
+
+# Open the provinces.txt file for reading.
+
+# Read the contents of the file into a list where each line of text in the file is stored in a separate element in the list.
+
+# Print the entire list.
+
+# Remove the first element from the list.
+
+# Remove the last element from the list.
+
+# Replace all occurrences of "AB" in the list with "Alberta".
+
+# Count the number of elements that are "Alberta" and print that number.
