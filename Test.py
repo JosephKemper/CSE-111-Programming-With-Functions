b25c15e7ec317cffd1d15f57c3a29263bd3dffed
JosephKemper <JosephKemper@gmail.com>
2022-09-29 20:49:45 -0600

    Finished 09 Checkpoint

--- Test.py
+++ Test.py
@@ -1,70 +1,74 @@
-# Example 5
+# Copyright 2020, Brigham Young University-Idaho. All rights reserved.
 
-import csv
-
+"""
+Write a Python program named provinces.py that reads the contents
+of provinces.txt into a list and then modifies the list.
+"""
 
 def main():
-    # Index of the phone number column
-    # in the dentists.csv file.
-    PHONE_INDEX = 2
+    # Read the contents of a text file named
+    # provinces.txt into a list named provinces_list.
+    provinces_list = read_list("provinces.txt")
 
-    # Read the contents of the dentists.csv into a
-    # compound dictionary named dentists_dict. Use
-    # the phone numbers as the keys in the dictionary.
-    dentists_dict = read_dict("dentists.csv", PHONE_INDEX)
+    # As a debugging aid, print the entire list.
+    print(provinces_list)
 
-    # Print the dentists compound dictionary.
-    print(dentists_dict)
+    # Remove the first element from the list.
+    provinces_list.pop(0)
+    #print(provinces_list)
+
+    # Remove the last element from the list.
+    provinces_list.pop()
+    #print(provinces_list)
+
+    # Replace all occurrrences of "AB" with "Alberta".
+    for i in range(len(provinces_list)):
+        if provinces_list[i] == "AB":
+            provinces_list[i] = "Alberta"
+    #print(provinces_list)
+
+    # Call the list.count method which will count the
+    # number of times that Alberta appears in the list.
+    count = provinces_list.count("Alberta")
+
+    print()
+    print(f"Alberta occurs {count} times in the modified list.")
 
 
-def read_dict(filename, key_column_index):
-    """Read the contents of a CSV file into a compound
-    dictionary and return the dictionary.
+def read_list(filename):
+    """Read the contents of a text file into a list
+    and return the list that contains the lines of text.
 
-    Parameters
-        filename: the name of the CSV file to read.
-        key_column_index: the index of the column
-            to use as the keys in the dictionary.
-    Return: a compound dictionary that contains
-        the contents of the CSV file.
+    Parameter filename: the name of the text file to read
+    Return: a list of strings
     """
-    # Create an empty dictionary that will
-    # store the data from the CSV file.
-    dictionary = {}
+    # Create an empty list that will store
+    # the lines of text from the text file.
+    text_list = []
 
-    # Open the CSV file for reading and store a reference
-    # to the opened file in a variable named csv_file.
-    with open(filename, "rt") as csv_file:
+    # Open the text file for reading and store a reference
+    # to the opened file in a variable named text_file.
+    with open(filename, "rt") as text_file:
 
-        # Use the csv module to create a reader object
-        # that will read from the opened CSV file.
-        reader = csv.reader(csv_file)
+        # Read the contents of the text
+        # file one line at a time.
+        for line in text_file:
 
-        # The first row of the CSV file contains column
-        # headings and not data, so this statement skips
-        # the first row of the CSV file.
-        next(reader)
+            # Remove white space, if there is any,
+            # from the beginning and end of the line.
+            clean_line = line.strip()
 
-        # Read the rows in the CSV file one row at a time.
-        # The reader object returns each row as a list.
-        for row_list in reader:
+            # Append the clean line of text
+            # onto the end of the list.
+            text_list.append(clean_line)
 
-            # If the current row is not blank, add the
-            # data from the current to the dictionary.
-            if len(row_list) != 0:
-
-                # From the current row, retrieve the data
-                # from the column that contains the key.
-                key = row_list[key_column_index]
-
-                # Store the data from the current
-                # row into the dictionary.
-                dictionary[key] = row_list
-
-    # Return the dictionary.
-    return dictionary
+    # Return the list that contains the lines of text.
+    return text_list
 
 
-# Call main to start this program.
+# If this file was executed like this:
+# > python teach_solution.py
+# then call the main function. However, if this file
+# was simply imported, then skip the call to main.
 if __name__ == "__main__":
     main()
