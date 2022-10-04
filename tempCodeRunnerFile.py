347e15dc703ccad0327fac95f9dcce84d2674294
JosephKemper <JosephKemper@gmail.com>
2022-10-03 19:21:30 -0600

    Worked on 10 Prove

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-# Import the csv module so that it can be used
+    # module so that it can be used in this program.
+    from datetime import datetime
+
+    # Call the now() method to get the current
+    # date and time as a datetime object from
+    # the computer's operating system.
+    current_date_and_time = datetime.now()
+
+    # Use an f-string to print the current
+    # day of the week and the current time.
+    print(f"{current_date_and_time:%A %I:%M %p}")
