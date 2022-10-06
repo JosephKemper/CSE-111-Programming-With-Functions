4ebd80a997608f597df6816a3a4e348bf5c7b814
JosephKemper <JosephKemper@gmail.com>
2022-10-05 20:10:04 -0600

    Finished L10 Prove

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,22 @@
-    # module so that it can be used in this program.
-    from datetime import datetime
-
-    # Call the now() method to get the current
-    # date and time as a datetime object from
-    # the computer's operating system.
-    current_date_and_time = datetime.now()
-
-    # Use an f-string to print the current
-    # day of the week and the current time.
-    print(f"{current_date_and_time:%A %I:%M %p}")
+    # except KeyError as Error:
+    #     # This code will be executed if the user tries to find 
+    #     # a product that is not in the dictionary or product list
+    #     print()
+    #     print(f"{Error}: unknown product ID in the filename file")
+    #     #print(product_code)
+        
+    # except FileNotFoundError as Error:
+    #     # This code will be executed if the user enters
+    #     # the name of a file that doesn't exist.
+    #     print()
+    #     print(f"{Error}: missing file")
+    #     print("[Errno 2] No such file or directory: ")
+    
+    # except PermissionError as perm_err:
+    #     # This code will be executed if the user enters the name
+    #     # of a file and doesn't have permission to read that file.
+    #     print()
+    #     print(type(perm_err).__name__, perm_err, sep=": ")
+    #     print("You don't have permission to read filename.")
+    #     print("Run the program again and enter the name" \
+    #             " of a file that you are allowed to read.")
