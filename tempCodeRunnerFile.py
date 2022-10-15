a6058113430d00d58de4ad14fa4aa9dfea25c638
JosephKemper <JosephKemper@gmail.com>
2022-10-14 20:00:13 -0600

    Finished L12 Checkpoint.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,22 +1,15 @@
-    # except KeyError as Error:
-    #     # This code will be executed if the user tries to find 
-    #     # a product that is not in the dictionary or product list
-    #     print()
-    #     print(f"{Error}: unknown product ID in the filename file")
-    #     #print(product_code)
-        
-    # except FileNotFoundError as Error:
-    #     # This code will be executed if the user enters
-    #     # the name of a file that doesn't exist.
-    #     print()
-    #     print(f"{Error}: missing file")
-    #     print("[Errno 2] No such file or directory: ")
-    
-    # except PermissionError as perm_err:
-    #     # This code will be executed if the user enters the name
-    #     # of a file and doesn't have permission to read that file.
-    #     print()
-    #     print(type(perm_err).__name__, perm_err, sep=": ")
-    #     print("You don't have permission to read filename.")
-    #     print("Run the program again and enter the name" \
-    #             " of a file that you are allowed to read.")
+    # # Add code to reverse and print fruit_list.
+    # fruit_list = fruit_list.reverse()
+    # print (fruit_list)
+
+    # # Add code to append "orange" to the end of fruit_list
+    # fruit_list.append ("orange")
+    # # print the list
+    # print (fruit_list)
+
+    # # Add code to find where "apple" is located in fruit_list 
+    # apple_index = fruit_list.index ("apple")
+    # # insert "cherry" before "apple" in the list 
+    # fruit_list.insert ("cherry",apple_index)
+    # # print the list.
+    # print (fruit_list)
