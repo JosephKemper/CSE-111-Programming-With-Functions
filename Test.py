7b06ed053dbd5b9d8b040eb84fe4fae263100cb4
JosephKemper <JosephKemper@gmail.com>
2022-10-19 20:07:45 -0600

    More work on text_adventure.py

--- Test.py
+++ Test.py
@@ -1,18 +1,27 @@
 import PySimpleGUI as sg
+import math
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Text('Calculate Volume of sphere')],
+            [sg.InputText(key='-NAME-')],
+            [sg.InputText(key='Radius')],
+            [sg.Submit(), sg.Cancel()] ]
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    print('You entered ', values[0])
+window = sg.Window ('Josephs First window', layout)
+
+event, values = window.read()
 
 window.close()
+
+def cal_sph_vol (radius):
+    volume = (4/3)*math.pi*(radius**3)
+    return volume
+
+float_radius = float(values['Radius'])
+sphere_volume = cal_sph_vol (float_radius)
+
+sg.popup("The volume is", sphere_volume)
+
+sg.popup(values['-NAME-'], 'entered', values["Radius"])
+
