c6e1422fca3c18a7eaccb35e03c156c456687680
JosephKemper <JosephKemper@gmail.com>
2022-10-22 12:03:40 -0600

    Syncing Work

--- text_adventure.py
+++ text_adventure.py
@@ -41,7 +41,54 @@ def character_creation ():
 
 # TODO: #61 Build functionality for Option 3 to be used. 
 
-# TODO: #56 Get the Character Creation window to close when I am done with it. 
+def cust_dia (gender, key):
+    """
+    The dialog in this adventure commonly uses 
+    gender references that are meant to be tailored 
+    directly to the player. The purpose of this function
+    is to allow those references to be dynamic to the character
+    which the player created.
+    """
+    male = {
+        "he_she" :"he", 
+        "him_her" : "him",
+        "his_her" : "his",
+        "boy_girl" : "boy",
+        "brother_sister" : "brother",
+        "son_daughter" : "son",
+        "male_female" : "male",
+        "men_women" : "men",
+        "man_woman" : "man",
+        "love_interest" : "girl",
+        "opposite_male_female" : "female",
+        "opposite_men_women" : "women",
+        "opposite_he_she" : "she",
+        "opposite_his_her" : "her",
+        "opposite_him_her" : "her",
+        "enemy_leader" : "Lady"
+        }
+
+    female = {
+        "he_she" : "she",
+        "him_her" : "her",
+        "his_her" : "her",
+        "boy_girl" : "girl",
+        "brother_sister" : "sister",
+        "son_daughter" : "daughter",
+        "male_female" : "female",
+        "men_women" : "women",
+        "man_woman" : "woman",
+        "love_interest" : "guy",
+        "opposite_male_female" : "male",
+        "opposite_men_women" : "men",
+        "opposite_he_she" : "he",
+        "opposite_his_her" : "his",
+        "opposite_him_her" : "him",
+        "enemy_leader" : "Lord"
+        }
+    return gender.get(key)
+
+
 # TODO: #57 Build first scene and figure out how I will build and print more scenes
 # TODO: #58 Figure out how I am going to organize my app. 
 
